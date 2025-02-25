d90da60fecf5183ed46d4ffed397a208a55d5f86
SteedStone <guerand12004@gmail.com>
2025-02-25 13:57:54 +0100

    okay

---
+++ VIsualisation/src/homework.c
@@ -0,0 +1,186 @@
+#include "fem.h"
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+void chk(int ierr) {
+    if (ierr != 0) {
+        printf("Error %d\n",ierr);
+        exit(1);
+    }
+}
+double hermiteInterpolation(double d, double dStar, double h0, double hStar) {
+    if (d <= 0) return h0;
+    if (d >= dStar) return hStar;
+    double t = d / dStar;
+    double t2 = t * t;
+    double t3 = t2 * t;
+    return h0 * (2 * t3 - 3 * t2 + 1) + hStar * (3 * t2 - 2 * t3);
+}
+
+double geoSize(double x, double y){
+
+    femGeo* theGeometry = geoGetGeometry();
+    
+    double h = theGeometry->h;
+    double x0 = theGeometry->xNotch;
+    double y0 = theGeometry->yNotch;
+    double r0 = theGeometry->rNotch;
+    double h0 = theGeometry->hNotch;
+    double d0 = theGeometry->dNotch;
+  
+    
+    double x1 = theGeometry->xHole;
+    double y1 = theGeometry->yHole;
+    double r1 = theGeometry->rHole;
+    double h1 = theGeometry->hHole;
+    double d1 = theGeometry->dHole;
+
+
+//
+//     A modifier !
+//     
+// Your contribution starts here ....
+//
+    
+    double distNotch = sqrt((x - x0) * (x - x0) + (y - y0) * (y - y0)) - r0;
+    double distHole = sqrt((x - x1) * (x - x1) + (y - y1) * (y - y1)) - r1;
+
+    // Interpolation Hermite pour chaque zone
+    double sizeNotch = hermiteInterpolation(distNotch, d0, h0, h);
+    double sizeHole = hermiteInterpolation(distHole, d1, h1, h);
+
+    // Prendre la taille minimale pour un meilleur raffinement
+    return fmin(sizeNotch, sizeHole);
+    
+//   
+// Your contribution ends here :-)
+//
+
+}
+
+
+#define ___ 0
+
+void geoMeshGenerate() {
+
+
+    
+ 
+//
+//  -1- Construction de la g�om�trie avec OpenCascade
+//      On cr�e le rectangle
+//      On cr�e les deux cercles
+//      On soustrait les cercles du rectangle :-)
+//
+ 
+    femGeo* theGeometry = geoGetGeometry();
+    
+    double hexRadius = theGeometry->hexRadius;  // Rayon de l'hexagone
+    int numHexX = 5;  // Nombre d'hexagones en largeur
+    int numHexY = 5;  // Nombre d'hexagones en hauteur
+    int ierr;
+    double meshSize = 0.1;
+    
+    int mainWireTags[100]; // Contient tous les hexagones extérieurs
+    int innerWireTags[100]; // Contient les petits hexagones à soustraire
+    int wireCount = 0, innerWireCount = 0;
+    
+    for (int i = 0; i < numHexX; i++) {
+        for (int j = 0; j < numHexY; j++) {
+            double x = i * 1.5 * hexRadius;
+            double y = j * sqrt(3) * hexRadius + (i % 2) * sqrt(3) * hexRadius / 2;
+            
+            int points[6], innerPoints[6];
+            for (int k = 0; k < 6; k++) {
+                double angle = M_PI / 3 * k;
+                double px = x + hexRadius * cos(angle);
+                double py = y + hexRadius * sin(angle);
+                points[k] = gmshModelOccAddPoint(px, py, 0, meshSize, -1, &ierr);
+                ErrorGmsh(ierr);
+                
+                double innerPx = x + (hexRadius * 0.8) * cos(angle);
+                double innerPy = y + (hexRadius * 0.8) * sin(angle);
+                innerPoints[k] = gmshModelOccAddPoint(innerPx, innerPy, 0, meshSize, -1, &ierr);
+                ErrorGmsh(ierr);
+            }
+            
+            int lines[6], innerLines[6];
+            for (int k = 0; k < 6; k++) {
+                lines[k] = gmshModelOccAddLine(points[k], points[(k + 1) % 6], -1, &ierr);
+                ErrorGmsh(ierr);
+                innerLines[k] = gmshModelOccAddLine(innerPoints[k], innerPoints[(k + 1) % 6], -1, &ierr);
+                ErrorGmsh(ierr);
+            }
+            
+            int wire = gmshModelOccAddWire(lines, 6, -1, 1, &ierr);
+            ErrorGmsh(ierr);
+            int innerWire = gmshModelOccAddWire(innerLines, 6, -1, 1, &ierr);
+            ErrorGmsh(ierr);
+            
+            mainWireTags[wireCount++] = wire;
+            innerWireTags[innerWireCount++] = -innerWire; // Ajoute les hexagones intérieurs en négatif
+        }
+    }
+    
+    
+    // Création d'un grand rectangle englobant la structure
+    int rectPoints[4];
+    rectPoints[0] = gmshModelOccAddPoint(-hexRadius, -hexRadius, 0, meshSize, -1, &ierr);
+    rectPoints[1] = gmshModelOccAddPoint(numHexX * 1.5 * hexRadius, -hexRadius, 0, meshSize, -1, &ierr);
+    rectPoints[2] = gmshModelOccAddPoint(numHexX * 1.5 * hexRadius, numHexY * sqrt(3) * hexRadius, 0, meshSize, -1, &ierr);
+    rectPoints[3] = gmshModelOccAddPoint(-hexRadius, numHexY * sqrt(3) * hexRadius, 0, meshSize, -1, &ierr);
+    
+    int rectLines[4];
+    for (int k = 0; k < 4; k++) {
+        rectLines[k] = gmshModelOccAddLine(rectPoints[k], rectPoints[(k + 1) % 4], -1, &ierr);
+        ErrorGmsh(ierr);
+    }
+    
+    int rectWire = gmshModelOccAddWire(rectLines, 4, -1, 1, &ierr);
+    ErrorGmsh(ierr);
+    
+    // Fusionner tous les contours (grande plaque - petits hexagones)
+    int allWires[200];
+    int totalWires = 0;
+    
+    // Ajouter la plaque principale
+    for (int i = 0; i < wireCount; i++)
+        allWires[totalWires++] = mainWireTags[i];
+
+    // Ajouter les petits hexagones à soustraire
+    for (int i = 0; i < innerWireCount; i++)
+        allWires[totalWires++] = innerWireTags[i];
+
+    // Création de la surface principale avec les trous hexagonaux
+    int surface = gmshModelOccAddPlaneSurface(allWires, totalWires, rectWire, &ierr);
+    ErrorGmsh(ierr);
+    
+    gmshModelOccSynchronize(&ierr);
+    gmshOptionSetNumber("Mesh.SaveAll", 1, &ierr);
+    gmshModelMeshGenerate(2, &ierr);
+//
+//  -2- D�finition de la fonction callback pour la taille de r�f�rence
+//      Synchronisation de OpenCascade avec gmsh
+//      G�n�ration du maillage (avec l'option Mesh.SaveAll :-)
+                  
+   
+       
+//
+//  Generation de quads :-)
+//
+//    gmshOptionSetNumber("Mesh.SaveAll", 1, &ierr);
+//    gmshOptionSetNumber("Mesh.RecombineAll", 1, &ierr);
+//    gmshOptionSetNumber("Mesh.Algorithm", 8, &ierr);  chk(ierr);
+//    gmshOptionSetNumber("Mesh.RecombinationAlgorithm", 1.0, &ierr);  chk(ierr);
+//    gmshModelGeoMeshSetRecombine(2,1,45,&ierr);  chk(ierr);
+//    gmshModelMeshGenerate(2, &ierr);  
+   
+ 
+// //
+// //  Plot of Fltk
+// //
+//   gmshFltkInitialize(&ierr);
+//   gmshFltkRun(&ierr);  chk(ierr);
+
+    
+}
